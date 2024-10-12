bdd7cab1b64868151e7317bb98a3acc915c25060
sebbe1236 <sebastiankvaalsvoll@gmail.com>
2024-10-12 13:16:34 +0200

    Added bootstrap css for styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { Observable, Subscription } from 'rxjs';
 import { SocketService } from './services/socket-service.service';
 import { io } from 'socket.io-client';
 import { ChatComponent } from './chat/chat.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChatComponent],
+  imports: [RouterOutlet, ChatComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -20,28 +20,4 @@ export class ChatComponent {
   ngOnInit() {
     this.getSocketStatus();
   }
-
-  // getMessages() {
-  //   this.socketService.onMessage().subscribe({
-  //     next: (message) => {
-  //       console.log('Received message from', message);
-  //       this.messages.push(`pushed ${message} into array`);
-  //     },
-  //     error: (error: any) => {
-  //       console.error('Error:', error);
-  //     },
-  //     complete: () => {
-  //       console.log('Connection closed, complete block');
-  //     },
-  //   });
-  // }
-
-  // sendMessage() {
-  //   this.socketService.sendMessage(this.message);
-  //   this.message = '';
-  // }
-
-  // ngOnInit() {
-  //   this.getMessages();
-  // }
 }
